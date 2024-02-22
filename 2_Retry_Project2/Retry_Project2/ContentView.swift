73b207876646ba9944c12d8fcb6a417f4103b346
dexter ramos <blitzdex27@gmail.com>
2024-02-22 22:03:16 +0800

    add retry for project two

---
+++ 2_Retry_Project2/Retry_Project2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Retry_Project2
+//
+//  Created by Dexter Ramos on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
