6981ef268116542d2ded1ce11b984f054a8537a3
dexter ramos <blitzdex27@gmail.com>
2024-02-28 23:50:23 +0800

    day 26

--- 6_Retry_Project3/6_Retry_Project3/ContentView.swift
+++ 6_Retry_Project3/6_Retry_Project3/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var date = Date.now
     var body: some View {
-        Stepper("Sleep Amount: \(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+        Text(Date.now.formatted(date: .numeric, time: .standard))
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//struct ContentView: View {
+//    @State private var sleepAmount = 8.0
+//    var body: some View {
+//        Stepper("Sleep Amount: \(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var date = Date.now
+//    var body: some View {
+//        DatePicker("Please select date:", selection: $date, in: date..., displayedComponents: .date)
+//            .labelsHidden()
+//    }
+//}
