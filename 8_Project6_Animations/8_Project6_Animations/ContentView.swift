8a569a1a41945b7f2f8cf5af06dc85b0e1006c4b
dexter ramos <blitzdex27@gmail.com>
2024-03-09 11:29:17 +0800

    update

--- 8_Project6_Animations/8_Project6_Animations/ContentView.swift
+++ 8_Project6_Animations/8_Project6_Animations/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .transition(.scale)
+                    .transition(.pivot)
             }
         }
         .onTapGesture {
