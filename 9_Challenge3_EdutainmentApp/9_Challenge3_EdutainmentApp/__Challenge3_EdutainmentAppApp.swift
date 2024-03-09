4de03644b02f49c65084e6d30b7018cbda08a874
dexter ramos <blitzdex27@gmail.com>
2024-03-09 21:31:03 +0800

    day 35 initial

---
+++ 9_Challenge3_EdutainmentApp/9_Challenge3_EdutainmentApp/__Challenge3_EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Challenge3_EdutainmentAppApp.swift
+//  9_Challenge3_EdutainmentApp
+//
+//  Created by Ss on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct __Challenge3_EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
