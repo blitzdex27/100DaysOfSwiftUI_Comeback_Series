8ba9f419eec0ae0373d29581bad87c9e14fe928e
dexter ramos <blitzdex27@gmail.com>
2024-03-10 20:17:27 +0800

    day 35 initial commit

---
+++ 9_Challenge3_MultiplicationTable/9_Challenge3_MultiplicationTable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  9_Challenge3_MultiplicationTable
+//
+//  Created by Dexter Ramos on 3/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 9_Challenge3_MultiplicationTable/9_Challenge3_MultiplicationTable/__Challenge3_MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Challenge3_MultiplicationTableApp.swift
+//  9_Challenge3_MultiplicationTable
+//
+//  Created by Dexter Ramos on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct __Challenge3_MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
