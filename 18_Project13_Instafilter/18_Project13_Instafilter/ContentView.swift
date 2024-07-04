b6f4ef625fba9911aa01004dd6caafe531bcd8af
dexter <blitzdex27@gmail.com>
2024-07-04 18:13:49 +0800

    save

---
+++ 18_Project13_Instafilter/18_Project13_Instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  18_Project13_Instafilter
+//
+//  Created by Dexter  on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 18_Project13_Instafilter/18_Project13_Instafilter/_8_Project13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_Project13_InstafilterApp.swift
+//  18_Project13_Instafilter
+//
+//  Created by Dexter  on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct _8_Project13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
