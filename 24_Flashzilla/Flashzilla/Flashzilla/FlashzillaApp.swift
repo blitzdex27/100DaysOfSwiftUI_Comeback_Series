3cb7655dd515bc6e4721f4b30fd3a85d69b357d1
dexter <blitzdex27@gmail.com>
2024-07-30 17:41:31 +0800

    update

---
+++ 24_Flashzilla/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Dexter  on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
