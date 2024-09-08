776ddd16c30e03f0419a404589cf077307036d27
dexter ramos <blitzdex27@gmail.com>
2024-09-09 00:17:51 +0800

    layout and geometry day 94

--- 25_LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 25_LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,28 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .blue, .green, .yellow, .purple, .pink, .orange]
+    
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(0..<100) { index in
-                        Text("Number \(index)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .frame(width: 200, height: 200)
-                            .visualEffect { content, proxy in
-                                content
-                                    .rotation3DEffect(
-                                        .degrees(-proxy.frame(in: .global).minX / 8),
-                                        axis: (0, 1, 0)
-                                    )
-                            }
+        
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row: \(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(
+                                    hue: min(proxy.frame(in: .global).midY / fullView.size.height, 0.8),
+                                    saturation: 0.5,
+                                    brightness: 1
+                                )
+                            )
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(
+                                
+                                {
+                                    let screenHeight = fullView.size.height
+                                    let screenMidY = screenHeight / 2
+                                    let midY = proxy.frame(in: .global).midY
+                                    
+                                    let midYOffset = midY - screenMidY
+                                    
+                                    let offsetPercentage = midYOffset / screenMidY
+                                    let scale = (offsetPercentage * 0.5)
+                                    
+                                    let scaleFactor = 1 + min(scale, 0.5)
+                                    
+                                    return scaleFactor
+                                }()
+                                
+                                
+                            )
+                            .rotation3DEffect(
+                                .degrees(proxy.frame(in: .global).minY - fullView.size.width / 2) / 5,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                            
+                    }
+                    .frame(height: 40)
                     
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
+        
+        
     }
 }
 
